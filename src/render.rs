954e817d8c30b8a1057126f5d6f3ab3e9e351088
nabil <nabil.bennacer@etu.umontpellier.fr>
2026-03-27 17:09:32 +0100

    goal scoring  management

--- src/render.rs
+++ src/render.rs
@@ -1,8 +1,69 @@
 use macroquad::prelude::*;
 use crate::models::ball::Ball;
 use crate::models::player::Player;
+use crate::game::{self, GameState, Match};
 
-pub fn draw_all(player: &Player, stadium_texture: &Texture2D, ball: &Ball, debug_hitbox: bool) {
+
+fn draw_score(game_match: &Match) {
+    let score_text = format!("{}  -  {}", game_match.score_p1, game_match.score_p2);
+    let font_size = 75.0; 
+    let text_params = measure_text(&score_text, None, font_size as u16, 1.0);
+    
+
+    let box_width = text_params.width + 100.0;
+    let box_height = text_params.height + 30.0;
+    let box_x = screen_width() / 2.0 - box_width / 2.0;
+    let box_y = 10.0;
+
+
+    draw_rectangle(
+        box_x + 5.0, box_y + 5.0, 
+        box_width, box_height, 
+        Color::new(0.0, 0.0, 0.0, 0.4)
+    );
+    
+ 
+    draw_rectangle(
+        box_x, box_y, 
+        box_width, box_height, 
+        Color::new(0.15, 0.15, 0.15, 1.0)
+    );
+    
+
+    draw_rectangle_lines(
+        box_x, box_y, 
+        box_width, box_height, 
+        4.0, // Épaisseur du trait
+        Color::new(0.7, 0.7, 0.7, 1.0)
+    );
+
+  
+    let text_x = screen_width() / 2.0 - text_params.width / 2.0;
+    let text_y = box_y + text_params.height + 8.0;
+
+   
+    draw_text(&score_text, text_x + 3.0, text_y + 3.0, font_size, BLACK);
+    
+    
+    draw_text(&score_text, text_x, text_y, font_size, WHITE);
+
+    //animation de but
+    if let GameState::GoalScored { .. } = game_match.state {
+        let goal_text = "GOAL !";
+        let goal_size = 140.0; 
+        let goal_params = measure_text(goal_text, None, goal_size as u16, 1.0);
+        
+        let text_x = screen_width() / 2.0 - goal_params.width / 2.0;
+        let text_y = screen_height() / 2.0 + goal_params.height / 2.0 - 50.0;
+
+        // Ombre portée très marquée pour le GOAL
+        draw_text(goal_text, text_x + 8.0, text_y + 8.0, goal_size, BLACK);
+        draw_text(goal_text, text_x, text_y, goal_size, YELLOW);
+    }
+}
+
+
+pub fn draw_all(player: &Player, stadium_texture: &Texture2D, ball: &Ball, debug_hitbox: bool, game_match: &Match) {
     draw_texture_ex(stadium_texture, 0.0, 0.0, WHITE, DrawTextureParams {
         dest_size: Some(vec2(screen_width(), screen_height())),
         ..Default::default()
@@ -42,6 +103,8 @@ pub fn draw_all(player: &Player, stadium_texture: &Texture2D, ball: &Ball, debug
             ..Default::default()
         },
     );
+
+    draw_score(game_match);
 
     if debug_hitbox {
         draw_debug_hitbox(player, ball);
